6a90c7675f2ac93f9e9db498a2b661af7c62bb7b
JRP-5 <jrp2020@outlook.com>
2025-07-20 19:09:19 +0100

    Working on monitoring cache misses

--- src/hook.cpp
+++ src/hook.cpp
@@ -6,23 +6,55 @@
 #include <iostream>
 #define UNW_LOCAL_ONLY
 #include <libunwind.h>
+#include <linux/perf_event.h>
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <cstdint>
+#include <thread>
+#include <chrono>
 
-void backtrace(){
-    fprintf(stderr, "backtracing");
+std::thread monitor_thread;
+void dump_backtrace(){
     const auto MAX_SIZE = 64;
-    fprintf(stderr, "0");
     std::vector<void *> trace(MAX_SIZE);
-    fprintf(stderr, "1");
     const auto size = unw_backtrace(trace.data(), MAX_SIZE);
-    fprintf(stderr, "2");
     trace.resize(size);
-    fprintf(stderr, "done");
     for(auto ip: trace){
         fprintf(stderr, "\tip: %p\n", ip);
     }
     
 }
+void monitor_cache_misses() {
+    struct perf_event_attr pe{};
+    pe.type = PERF_TYPE_HARDWARE;
+    pe.size = sizeof(struct perf_event_attr);
+    pe.config = PERF_COUNT_HW_CACHE_MISSES;
+    pe.disabled = 1;
+    pe.exclude_kernel = 1;
+    pe.exclude_hv = 1;
 
+    int fd = syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
+    if (fd == -1) {
+        perror("perf_event_open");
+        return;
+    }
+
+    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+}
+void monitor_loop() {
+    while (true) {
+        monitor_cache_misses(); 
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+__attribute__((constructor))
+static void on_load() {
+    fprintf(stderr, "[memprofiler] Loaded via LD_PRELOAD\n");
+    // Start background perf monitor
+    monitor_thread = std::thread(monitor_loop);
+}
 extern "C"
 {   
     void* malloc(size_t size)
@@ -31,19 +63,27 @@ extern "C"
         assert(original_malloc);
         auto *original_malloc_fn = reinterpret_cast<decltype(&::malloc)>(original_malloc);
         
+        // Stop infinite recursion
         thread_local bool reentrant = false;
         if (reentrant) {
             return original_malloc_fn(size);
         }
         reentrant = true;
 
-
-        void *ret = original_malloc_fn(size);
-        
-        backtrace();
-        
+        void *ret = original_malloc_fn(size);        
+        dump_backtrace();
         fprintf(stderr, "malloc intercepted: %zu -> %p\n", size, ret);
         reentrant = false;
+        
         return ret;
     }
+}
+
+int main() {
+    return 0;
+}
+
+__attribute__((destructor))
+static void on_unload() {
+    monitor_thread.join();
 }
