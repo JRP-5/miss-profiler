fa21fdca0d643b693ada8a787bda9d985766c2c1
JRP-5 <jrp2020@outlook.com>
2025-08-12 22:57:07 +0100

    Slowly working on resolving address

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-ADDR_IN_FILE=$(printf  "%#X\n" $(( 0x58c42d479245 - 0x58c42d478000 )))
-eu-addr2line -f -C -e ./bin/vector "$ADDR_IN_FILE"
-sudo LD_PRELOAD=./lib/libhook.so ./bin/vector
+# printf  "%#X\n" $(( 0x5ae5c21302a5 - 0x5ae5c212c000 ))
+ADDR_IN_FILE=$(printf  "%#X\n" $(( 0x5ab1f6dd62a5 - 0x5ab1f6dd2000 )))
+addr2line -f -p -C -e ./bin/vector "$ADDR_IN_FILE" 
